9488ebddccb785fad0d05205a3092e91b9a4b1f7
hua1995116 <461249104@qq.com>
2017-12-29 18:36:57 +0800

    '更新地图'

--- parse.js
+++ parse.js
@@ -1,5 +1,7 @@
+const axios = require('axios');
 const filetools = require('./utils/filetools.js');
 const {readFile, writeFile} = filetools; 
+const separate = require('./utils/separate.js');
 const mailer = require('./utils/mailto.js');
 const debounce = require('./utils/index.js').debounce;
 
@@ -19,12 +21,13 @@ module.exports = function parse(time) {
     const PARSE_PVUV_URL = `./charts/${config.csvDir}/${time}PVUV.csv`;
     const PARSE_TIME_URL = `./charts/${config.csvDir}/${time}time.csv`;
     const PARSE_MOBILE_URL = `./charts/${config.csvDir}/${time}mobile.csv`;
+    const PARSE_MAP_URL = `./charts/${config.csvDir}/${time}map.json`;
 
 
     readFile(ORIGINAL_DATA_URL).then((data) => {
         const jsondata = parseData(data)
-        parseTime(data);
-        parseMobile(jsondata);
+        // parseTime(data);
+        // parseMobile(jsondata);
         parsePVUV(jsondata);
         // fs.writeFile(`${config.jsonDir}/${config.date}data.json`, JSON.stringify(jsondata), (err) => {
         //     if(err) {
@@ -38,7 +41,7 @@ module.exports = function parse(time) {
     })
     
     readFile(ERROR_DATA_URL).then((data) => {
-        parseError(data);
+        // parseError(data);
     })
     
 
@@ -235,13 +238,167 @@ module.exports = function parse(time) {
         });
         // 合并对象后的长度
         const UV = Object.keys(obj).length;
+
+        mapData(obj, UV, `PV:${PV},UV:${UV}`);
+    }
+
+    function writePVUV(data) {
         // 写入文件
-        writeFile(PARSE_PVUV_URL, `PV:${PV},UV:${UV}`).then(res => {
+        writeFile(PARSE_PVUV_URL, data).then(res => {
             if(res === 200) {
                 console.log('PVUV success');
             }
         }).catch(err=> {
             console.log(`PVUV ${err}`);
+        })
+    }
+    /**
+     * 
+     * 
+     * @param {Object} newData
+     * {
+     *  "province": [{name: String, value: Nubmer}],
+     *  "city":[{name: String, value: Nubmer}]
+     * } 
+     */
+    function mapData(newData, newDataLength, PVUV) {
+        readFile(PARSE_PVUV_URL).then((PVData) => {
+            // 判断逻辑
+            const oldDataLength = PVData.split(',')[1].split(':')[1];
+            console.log(oldDataLength)
+            parseMap(oldDataLength, newDataLength, newData, PVUV)
+        }, err => {
+            fetchAddress(newData).then(Place => {
+                // const Place = [ '北京市', '广东省深圳市'];  
+                const newParse = parsePlace(Place);
+                console.log(newParse);
+                const mergeData = mergeHandle([], newParse);
+                const writeData = parsetoJson(mergeData);
+                console.log(writeData);
+                return writeFile(PARSE_MAP_URL,JSON.stringify(writeData))
+            }).then((res) => {
+                console.log('map1 success')
+                writePVUV(PVUV);
+            })
+        }).catch((err) => {
+            console.log(err);
+        })
+    } 
+
+    function parseMap(oldDataLength, newDataLength, newData, PVUV) {
+        console.log(oldDataLength, newDataLength, newData)
+        if(newDataLength > oldDataLength) {
+            const diff = diffDataMap(newData, oldDataLength);
+            console.log(diff);
+            readFile(PARSE_MAP_URL).then((json) => {
+                console.log('json'+json)
+                fetchAddress(diff).then(diffPlace => {
+                    console.log('diff'+diffPlace);
+                    const newParse = parsePlace(diffPlace);
+                    console.log('newparse'+JSON.stringify(newParse));
+                    const mergeData = mergeHandle(JSON.parse(json), newParse);
+                    console.log('merge'+ JSON.stringify(mergeData));
+                    const writeData = parsetoJson(mergeData);
+                    return writeFile(PARSE_MAP_URL,JSON.stringify(writeData))
+                }).then((res) => {
+                    console.log('map2 success');
+                    writePVUV(PVUV);
+                }).catch(err=> {
+                    console.log('fetch err' + err);
+                })
+            })
+        }
+    }
+
+    function mergeHandle(oldData, newData) {
+        // console.log(JSON.parse(oldData));
+        const Place = {};
+        for(var i in oldData) {
+          Place[oldData[i].name] = Place[oldData[i].name] ? Place[oldData[i].name] + 1 : 1;
+        } 
+        console.log('city'+ JSON.stringify(newData.city));
+        for(var i in newData.city) {
+          Place[i] = Place[i] ? parseInt(Place[i]) + parseInt(newData.city[i]) : parseInt(newData.city[i]);
+        }
+        console.log('place'+ JSON.stringify(Place));
+        return Place;
+    }
+
+    function diffDataMap(newData, oldDataLength) {
+        let j = 1;
+        let obj = {};
+        for(var i in newData) {
+            if(j > oldDataLength) {
+                obj[i] = newData[i]
+            }
+            j++;
+        }
+        return obj;
+    }
+
+    function fetchAddress(obj) {
+        return new Promise((resolve, reject) => {
+            const fetchArr = Object.keys(obj);
+            const arr = [];
+            const place = {};
+            for(let i = 0; i < fetchArr.length; i++) {
+                // console.log(fetchArr[i]);
+                arr.push(function() {
+                    return new Promise((resolve, reject) => {
+                        // http://apis.juhe.cn/ip/ip2addr?ip=${fetchArr[i]}&key=cfa7dff97a88306ff5b49d6941d51a3e 
+                        axios.get(`http://ip.soshoulu.com/ajax/shoulu.ashx?_type=ipsearch&ip=${fetchArr[i]}&px=1`).then((res) => {
+                            // const area = res.data.result.area;
+                            const area = res.data.replace(/\$.+/,'').trim()
+                            // console.log(JSON.stringify(area));
+                            place[area] = place[area] ? place[area]+1 : 1;
+                            resolve(area);
+                        }).catch((err) => {
+                            reject(err);
+                        })
+                    }) 
+                });
+            }
+            Promise.all(arr.map(item => item())).then((res) => {
+                console.log(res);
+                resolve(res);
+            }).catch(err=>{
+                console.log('all error' + err)
+            })
+        })
+    }
+    /**
+     * 
+     * 
+     * @param {Object} obj 
+     * @returns 
+     * {
+     *  city: {Name: Number}
+     * }
+     */
+    function parsePlace(obj) {
+        const parseObj = {
+            city: {},
+        };
+        obj = separate(obj);
+        for(var i in obj) {
+            // const province = obj[i].split('省')[0];
+            let city = obj[i];
+            // if(obj[i].split('省').length > 1) {
+            //     city = obj[i].split('省')[1].replace('市', '');
+            // } else {
+            //     city = obj[i].replace('市', '');
+            // }
+            parseObj['city'][city] = parseObj['city'][city] ? parseObj['city'][city]+1 : 1;
+        }
+        return parseObj;
+
+    }
+    function parsetoJson(obj) {
+        return Object.keys(obj).map((item) => {
+            return {
+                name: item,
+                value: obj[item]
+            }
         })
     }
 };
