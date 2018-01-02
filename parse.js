54c5bb972231549b38bd673aa5f808e82cc48ebd
hua1995116 <461249104@qq.com>
2018-01-02 21:48:36 +0800

    '更新parse'

--- parse.js
+++ parse.js
@@ -58,6 +58,7 @@ module.exports = function parse(time) {
         //     return a.nowTime - b.nowTime;
         // })
         data.forEach((value, index, array) => {
+            value = value.replace('\r', '');
             if(!!value) {
                 const obj = {};
                 value = value.split('&');
